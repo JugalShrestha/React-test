8c0f2c8b318ce5de6c8fbb142a8ac93677001659
Jugal Shrestha <jugalnepali@gmail.com>
2023-08-20 11:06:45 +0545

    add: rootBranch in  items.tsx

--- src/components/variables/items.tsx
+++ src/components/variables/items.tsx
@@ -1,11 +1,17 @@
 //All the available items in the website
 
+import rootBranch from "./rootBranch";
+
 const items = [
-  { name: "coverpage generator", tag: "project", link: "/coverpage-generator" },
-  { name: "text-2-handwriting", tag: "project", link: "/text2hw" },
-  { name: "syllabus", tag: "syllabus", link: "/syllabus" },
-  { name: "class routine", tag: "routine", link: "/routine" },
-  { name: "java course", tag: "note", link: "/java-course" },
+  {
+    name: "coverpage generator",
+    tag: "project",
+    link: rootBranch + "/coverpage-generator",
+  },
+  { name: "text-2-handwriting", tag: "project", link: rootBranch + "/text2hw" },
+  { name: "syllabus", tag: "syllabus", link: rootBranch + "/syllabus" },
+  { name: "class routine", tag: "routine", link: rootBranch + "/routine" },
+  { name: "java course", tag: "note", link: rootBranch + "/java-course" },
 ];
 
 //For sorting searchable items
